35f0d9313b05105f1ab6c990b23a0dd6063903c9
Shannen Saikia <shannen.saikia@gmail.com>
2026-08-09 21:56:42 +0530

    Wire grid background into the authenticated app shell

--- frontend/src/components/common/Layout.tsx
+++ frontend/src/components/common/Layout.tsx
@@ -8,7 +8,8 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="flex h-screen bg-gray-50">
+    <div className="relative flex h-screen">
+      <div className="app-grid-bg" />
       <Sidebar />
       <div className="flex-1 flex flex-col overflow-hidden">
         <Header />
